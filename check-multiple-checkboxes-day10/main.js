ea942ef37830caeadc6bdf3e828068425cf9c954
Carlos <crodriguezq91@gmail.com>
2017-09-10 14:16:33 -0500

    Project days 10 nd 11

--- check-multiple-checkboxes-day10/main.js
+++ check-multiple-checkboxes-day10/main.js
@@ -1,6 +1,26 @@
 
-const group = document.querySelectorAll('.inbox');
+const group = document.querySelectorAll('.inbox input[type="checkbox"]');
 
-group.addEventListener('onclick', () => {
-  console.log(group);
-})
+let lastChecked;
+
+function handleCheck(e) {
+  // Check if they had the shift key down
+  // AND check that they are chekcing it
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    group.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log('Starting check inbetween');
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+group.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
